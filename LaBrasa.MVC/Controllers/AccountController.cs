9145ded4e8cae6704a2e3b209b3c047d1e85fbc7
lucastteles <98498654+lucastteles@users.noreply.github.com>
2023-01-17 16:26:32 -0300

    Alterando ViewModel

--- LaBrasa.MVC/Controllers/AccountController.cs
+++ LaBrasa.MVC/Controllers/AccountController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LaBrasa.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LaBrasa.MVC.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
         {
-            return View();
+            _authenticate = authenticate;
+        }
+         
+        [HttpGet] 
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LOn;
         }
     }
 }
