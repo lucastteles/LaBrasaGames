d7c1dcb89fa6d8cb34d63daa0d6ed16d5bb200e3
lucastteles <98498654+lucastteles@users.noreply.github.com>
2023-03-09 17:49:16 -0300

    Adicionando Js Carroussel

--- LaBrasa.MVC/wwwroot/js/site.js
+++ LaBrasa.MVC/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const carousel = document.querySelector('.carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let position = 0; // inicialize a posição do carrossel
+
+prevButton.addEventListener('click', () => {
+    position += carousel.offsetWidth; // adicione a largura do carrossel para mover para a esquerda
+    carousel.style.transform = `translateX(${position}px)`; // ajuste a posição do carrossel
+});
+
+nextButton.addEventListener('click', () => {
+    position -= carousel.offsetWidth; // subtraia a largura do carrossel para mover para a direita
+    carousel.style.transform = `translateX(${position}px)`; // ajuste a posição do carrossel
+});
